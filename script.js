71a0b4486f1944fb4418a5b7c48c6458d92246e8
gaby-mvi <gaby.rosa.mvi@gmail.com>
2023-11-30 11:35:29 -0300

    Alteração do audio no scrit.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
     let formulario = document.querySelector(".fale-conosco")
     let mascara = document.querySelector(".mascara-form")
-    let audioFaleConosco = document.querySelector("fale-conocsco-audio")
-    let audioEnviarForm = document.querySelector("enviar-form")
+    let audioFaleConosco = document.querySelector(".fale-conosco-audio")
+    let audioEnviarForm = document.querySelector(".enviar-form")
 
 
     function cliqueiNoBotao() {
