91bb1f6bf5671f382b82f52ce5f65b3d11453ab1
gaby-mvi <gaby.rosa.mvi@gmail.com>
2023-11-30 11:13:23 -0300

    Alteração para site ficar responsivo

---
+++ script.js
@@ -0,0 +1,18 @@
+    let formulario = document.querySelector(".fale-conosco")
+    let mascara = document.querySelector(".mascara-form")
+    let audioFaleConosco = document.querySelector("fale-conocsco-audio")
+    let audioEnviaForm = document.querySelector("enviar-form")
+
+
+    function cliqueiNoBotao() {
+        formulario.style.left = "50%";
+        formulario.style.transform = "translateX(-50%)";
+        mascara.style.visibility = "visible";
+        audioFaleConosco.play();
+    }
+
+    function sumirFormulario() {
+        formulario.style.left = "-345px"
+        mascara.style.visibility = "hidden"
+        audioEnviarForm.play();
+    }
